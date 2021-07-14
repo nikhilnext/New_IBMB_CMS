3d23fd04620dac2da033393ec8b3ef9308bf10a1
nikhilnext <nikhilkarekr1@gmail.com>
2021-07-14 18:06:13 +0530

    writelog and deals api

---
+++ App_Code/commonfunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for commonfunction
+/// </summary>
+public class commonfunction
+{
+    public commonfunction()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+
+    public static void WriteLog(string FunctionName, string Parameters, DateTime Start, DateTime End, string Message = "")
+    {
+        DateTime TimeStamp = DateTime.UtcNow;
+        try
+        {
+            TimeSpan duration = Start - End;
+            string strPath = "~/Yes_Logs";
+            if (!Directory.Exists(HttpContext.Current.Server.MapPath(strPath)))
+            {
+                Directory.CreateDirectory(HttpContext.Current.Server.MapPath(strPath));
+            }
+            strPath = strPath + "/Logs_" + DateTime.Now.ToString("yyyy_MM");
+            if (!Directory.Exists(HttpContext.Current.Server.MapPath(strPath)))
+            {
+                Directory.CreateDirectory(HttpContext.Current.Server.MapPath(strPath));
+            }
+            strPath = strPath + "/LOG_" + DateTime.Now.ToString("yyyy_MM_dd") + ".txt";
+
+            string strLog = string.Format("LogTime :- {0}\r\nFunation_Name : {1}\r\nParameters : {2}\r\nStart Time : {3}\r\nEnd : {4}\r\nDuration : {5}\r\nMessage : {6}\r\n\r\n\r\n", DateTime.Now, FunctionName, Parameters, Start, End, duration, Message);
+            File.AppendAllText(HttpContext.Current.Server.MapPath(strPath), strLog);
+
+
+        }
+        catch (Exception)
+        {
+            //throw exp;
+        }
+    }
+}
--- deals.aspx.cs
+++ deals.aspx.cs
@@ -4,11 +4,61 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
 
 public partial class deals : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+
+    }
+    [WebMethod(EnableSession = true)]
+    public static string GetDealData(string DealType, string Sector)
+    {
+
+       
+        string DealJson = string.Empty;
+        string flag = string.Empty;
+        var conn = System.Configuration.ConfigurationManager.ConnectionStrings["dbStringnew"].ConnectionString;
+        try
+        {
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.CommandText = "dbo.GetDealData";
+                    cmd.Parameters.AddWithValue("@dealtype", SqlDbType.VarChar).Value = DealType;
+                    cmd.Parameters.AddWithValue("@sector", SqlDbType.VarChar).Value = Sector;
+                     
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                      //  DealJson = Serialization.RsJsonSerializer.JsonSerializerObjectHd(ds);
+                        DealJson = JsonConvert.SerializeObject(ds);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // LogError.Log_Err("Deals Data Error", ex, LogLevel.Critical);
+           commonfunction.WriteLog("GetDealData exception", ex.ToString(), DateTime.Now,DateTime.Now);
+            return "Error 404";
+        }
+        //return DealJson;
+        return DealJson;
     }
 }
+
+
+
